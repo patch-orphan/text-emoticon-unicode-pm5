43dac6a3d120bbedc83009fec596780a65610aa4
Nick Patch <patch@cpan.org>
2012-01-22 21:44:27 -0500

    new project

---
+++ lib/Text/Emoticon/Unicode.pm
@@ -0,0 +1,43 @@
+package Text::Emoticon::Unicode;
+
+use 5.006;
+use strict;
+use warnings;
+use utf8;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Text::Emoticon::Unicode - Text::Emoticon filter for Unicode emoticons
+
+=head1 VERSION
+
+This document describes Text::Emoticon::Unicode version 0.01.
+
+=head1 SYNOPSIS
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 AUTHOR
+
+Nick Patch <patch@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+© 2012 Nick Patch
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
