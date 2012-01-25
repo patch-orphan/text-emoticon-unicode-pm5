3e8a8f263175ca1b9379bcfe710e7983d470eec1
Nick Patch <patch@cpan.org>
2012-01-25 07:43:35 -0500

    release 0.01

--- lib/Text/Emoticon/Unicode.pm
+++ lib/Text/Emoticon/Unicode.pm
@@ -50,8 +50,8 @@ This document describes Text::Emoticon::Unicode version 0.01.
 
 =head1 DESCRIPTION
 
-This is a L<Text::Emoticon> filter for converting ASCII emoticons to thier
-Unicode emoticon character equivilent.  Unlike the other Text::Emoticon
+This is a L<Text::Emoticon> filter for converting ASCII emoticons to their
+Unicode emoticon character equivalent.  Unlike the other Text::Emoticon
 filters, this one does not use images from an external site or even use any
 HTML.  Instead it replaces a series of characters like C<:-)> with a single
 character like C<☺>.
@@ -59,8 +59,8 @@ character like C<☺>.
 Currently only the Unicode 1.1 emoticons C<☺> and C<☹> are supported, but an
 option to enable the vast range of Unicode 6.0 emoticons will be added in a
 future version.  An option to use numeric character references for HTML or XML
-will also be added, but it's easy to use L<HTML::Entities> or L<XML::Entities>
-for now.
+will also be added, but it’s easy to use L<HTML::Entities> or L<XML::Entities>
+instead.
 
 =head1 AUTHOR
 
